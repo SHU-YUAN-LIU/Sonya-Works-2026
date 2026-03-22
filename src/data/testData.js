628e7a69b5f13dcd9af28bb9e8d609fc70041f1c
Sonya-Liu <Sonya.Liu@syner-g.com.tw>
2026-03-22 23:00:15 +0800

    fix dashboard

--- src/data/testData.js
+++ src/data/testData.js
@@ -1,19 +1,29 @@
-export const worksData = [
-    {
-        title:"節能系統",
-        imgUrl:""
-    },
-    {
-        title:"計算碳排",
-        imgUrl:""
-    },
-    {
-        title:"個人專題(緯育)",
-        imgUrl:""
-    },
-    {
-        title:"團體專題(緯育)",
-        imgUrl:""
-    },
+import youthWantToEatParty from "@/assets/images/happyParty.jpg";
+import energySaving from "@/assets/images/energySaving.jpg";
+import MBMS from "@/assets/images/MBMS.jpg";
+import wolfer from "@/assets/images/wolfer.jpg";
 
-]
+export const worksData = [
+  {
+    title: "節能系統",
+    linkUrl: "https://energy-saving.synerg-data.net/login",
+    imgUrl: energySaving,
+  },
+  {
+    title: "能源系統",
+    linkUrl: "https://mbms-qa.synerg-data.com/login",
+    imgUrl: MBMS,
+  },
+  {
+    title: "個人專題(緯育)",
+    linkUrl: "https://shu-yuan-liu.github.io/CHD104-v1/index.html#goToTop-a",
+    imgUrl: wolfer,
+  },
+  {
+    title: "團體專題(緯育)",
+    linkUrl: "https://tibamef2e.com/chd104/g2/",
+    imgUrl: youthWantToEatParty,
+  },
+];
+
+  
